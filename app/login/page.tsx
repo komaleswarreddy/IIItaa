c13fddf74dff3f51e102dec86f57940b88bfb8c3
Komaleswar <n210038@rguktn.ac.in>
2025-01-28 08:20:52 -0500

    mine

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,6 +8,8 @@ import { useMotionValue } from 'framer-motion';
 export default function LoginPage() {
   const mouseX = useMotionValue(0);
   const mouseY = useMotionValue(0);
+  const [isTyping, setIsTyping] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const rect = e.currentTarget.getBoundingClientRect();
@@ -27,12 +29,14 @@ export default function LoginPage() {
         <InteractiveShapes 
           mouseX={mouseX} 
           mouseY={mouseY} 
-          isTyping={false}
-          isPassword={false}
+          isTyping={isTyping}
+          isPassword={isPassword}
         />
       </div>
-      <div className="z-10 w-full">
-        <LoginForm />
+      <div className="z-10 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-center">
+          <LoginForm />
+        </div>
       </div>
     </main>
   );
